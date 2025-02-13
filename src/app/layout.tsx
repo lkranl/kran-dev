57a2717f0ba605e8463a86842096e6d3f85ef567
kran <thekran95@gmail.com>
2025-02-12 23:14:23 -0500

    added google adsense to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kran's Website",
   description: "This is the base for the personal website of kran",
+  other: { googleAdsenseAccount: "ca-pub-3460906822050782" },
 };
 
 export default function RootLayout({
