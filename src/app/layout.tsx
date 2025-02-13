fcdd286759b7e629d9795d45f93ea65ef8f1d2c8
kran <thekran95@gmail.com>
2025-02-12 23:17:16 -0500

    modified google metadata name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kran's Website",
   description: "This is the base for the personal website of kran",
-  other: { googleAdsenseAccount: "ca-pub-3460906822050782" },
+  other: { "google-adsense-account": "ca-pub-3460906822050782" },
 };
 
 export default function RootLayout({
